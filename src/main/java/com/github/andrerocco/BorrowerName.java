451ac6e4a46bd440b7d1675cbdcde5e9047bafc8
Andre Rocco <99764757+andrerocco@users.noreply.github.com>
2024-03-18 23:04:35 -0300

    Primeiro commit

---
+++ src/main/java/com/github/andrerocco/BorrowerName.java
@@ -0,0 +1,31 @@
+package com.github.andrerocco;
+
+public class BorrowerName {
+    private String firstName;
+    private String lastName;
+
+    public BorrowerName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return firstName.toUpperCase() + " " + lastName.toUpperCase();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/test/java/com/github/andrerocco/BorrowerNameTest.java
@@ -0,0 +1,25 @@
+package com.github.andrerocco;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BorrowerNameTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getFullName() {
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+}
